ff4dc0b5ca321a84192803ae6040768b05e82ab1
crazytan <tjtanjia.tan@gmail.com>
2015-12-23 11:31:42 +0800

    complete div & mod

--- src/simpl/parser/ast/Div.java
+++ src/simpl/parser/ast/Div.java
@@ -17,7 +17,10 @@ public class Div extends ArithExpr {
 
     @Override
     public Value eval(State s) throws RuntimeError {
-        // TODO
-        return null;
+        IntValue v1 = (IntValue) l.eval(s);
+        IntValue v2 = (IntValue) r.eval(s);
+        if (v2.n == 0)
+            throw new RuntimeError("divide by zero");
+        return new IntValue(v1.n / v2.n);
     }
 }
